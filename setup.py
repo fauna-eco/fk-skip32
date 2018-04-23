4754b9c7c6f359aff9b213060aa6dcca40b4038c
franksomething <frank@sporkware.com>
2018-04-23 12:12:48 -0400

    update setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 module1 = Extension('skip32', sources = ['skip32/skip32.c'])
 
-setup (name = 'skip32',
-        version = '1.0',
-        description = 'implementation of skip32 based on http://search.cpan.org/~esh/Crypt-Skip32-0.08/lib/Crypt/Skip32.pm',
-        ext_modules = [module1],
-        author = 'Keith Bussell')
+setup (
+    name = 'fk-skip32',
+    version = '1.0',
+    description = 'implementation of skip32 based on http://search.cpan.org/~esh/Crypt-Skip32-0.08/lib/Crypt/Skip32.pm',
+    url='https://github.com/fictivekin/fk-skip32',
+    ext_modules = [module1],
+    author = 'Keith Bussell',
+    maintainer='Fictive Kin, LLC',
+    maintainer_email='frank@fictivekin.com',
+)
